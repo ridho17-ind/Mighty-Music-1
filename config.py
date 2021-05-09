237b97f87d8942988f03f587c36ec0db092e8b56
Yunus-ZEND <79404390+Yunus-ZEND@users.noreply.github.com>
2021-05-09 08:11:59 +0700

    Update config.py

--- config.py
+++ config.py
@@ -32,4 +32,4 @@ DURATION_LIMIT = int(getenv("DURATION_LIMIT", "10"))
 
 COMMAND_PREFIXES = list(getenv("COMMAND_PREFIXES", "/ !").split())
 
-SUDO_USERS = list(map(int, getenv("SUDO_USERS").split()))
+SUDO_USERS = list(map(int, getenv("SUDO_USERS").split())) 
