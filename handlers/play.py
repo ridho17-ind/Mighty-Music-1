272cb627a16a9119432c01f1afee791af4b04ad5
Yunus-ZEND <79404390+Yunus-ZEND@users.noreply.github.com>
2021-05-09 09:14:31 +0700

    Update play.py

--- handlers/play.py
+++ handlers/play.py
@@ -155,8 +155,8 @@ async def playlist(client, message):
     now_playing = temp[0][0]
     by = temp[0][1].mention(style='md')
     msg = "**Lagu Yang Sedang dimainkan** di {}".format(message.chat.title)
-    msg += "\n- "+ now_playing
-    msg += "\n- Req by "+by
+    msg += "\n🎵 Judul: "+ now_playing
+    msg += "\n🧸 Request dari: "+by
     temp.pop(0)
     if temp:
         msg += '\n\n'
@@ -164,8 +164,8 @@ async def playlist(client, message):
         for song in temp:
             name = song[0]
             usr = song[1].mention(style='md')
-            msg += f'\n- {name}'
-            msg += f'\n- Req by {usr}\n'
+            msg += f'\n🎵 {name}'
+            msg += f'\n🧸 Request dari: {usr}\n'
     await message.reply_text(msg)       
     
 # ============================= Settings =========================================
