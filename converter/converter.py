adc2dcfbbfb95e783a101d41ff94c44a48d343d8
Yunus-ZEND <79404390+Yunus-ZEND@users.noreply.github.com>
2021-05-09 08:06:41 +0700

    Update converter.py

--- converter/converter.py
+++ converter/converter.py
@@ -46,4 +46,4 @@ async def convert(file_path: str) -> str:
 
         return out
     except:
-        raise FFmpegReturnCodeError("FFmpeg did not return 0")
+        raise FFmpegReturnCodeError("FFmpeg did not return 0") 
