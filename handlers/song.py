b3b1ef5923f7d966d7bf074609307cfaa0cba01f
Yunus-ZEND <79404390+Yunus-ZEND@users.noreply.github.com>
2021-05-09 08:01:54 +0700

    Update song.py

--- handlers/song.py
+++ handlers/song.py
@@ -49,7 +49,7 @@ def song(client, message):
     for i in message.command[1:]:
         query += ' ' + str(i)
     print(query)
-    m = message.reply('🔎 Sedang Mencari Lagu...')
+    m = message.reply('🔎 Sedang Mencari Lagu tersebut...')
     ydl_opts = {"format": "bestaudio[ext=m4a]"}
     try:
         results = YoutubeSearch(query, max_results=1).to_dict()
@@ -78,7 +78,7 @@ def song(client, message):
             info_dict = ydl.extract_info(link, download=False)
             audio_file = ydl.prepare_filename(info_dict)
             ydl.process_info(info_dict)
-        rep = '**🎵 Uploaded by ** @mrismanaziz'
+        rep = '**🎵 Uploaded by ** @ZendYNS'
         secmul, dur, dur_arr = 1, 0, duration.split(':')
         for i in range(len(dur_arr)-1, -1, -1):
             dur += (int(dur_arr[i]) * secmul)
